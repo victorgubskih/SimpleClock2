c7f66381db0913bd7512cd55fe0f2c75e1ed3f65
victorgubskih <112268920+victorgubskih@users.noreply.github.com>
2022-12-14 17:15:56 +0200

    make 12 numbers around the circle

--- SimpleClock2/ViewController.swift
+++ SimpleClock2/ViewController.swift
@@ -23,8 +23,41 @@ class ViewController: UIViewController {
         sipleclock2.layer.borderColor = UIColor.black.cgColor
         
         sipleclock2.layer.cornerRadius = sipleclock2.frame.height / 2
+        
+        createOblectAraundCircle()
     }
 
-
+    func createOblectAraundCircle() {
+        
+        let centre = CGPoint(x: sipleclock2.bounds.width / 2, y: sipleclock2.bounds.height / 2)
+        let radius: CGFloat = 100
+        let count = 12
+        
+        var angle = CGFloat(2 * Double.pi - Double.pi / 3)
+        let step = CGFloat( 2 * Double.pi) / CGFloat(count)
+        
+        for index in 0..<count {
+            
+            let x = cos(angle) * radius + centre.x
+            let y = sin(angle) * radius + centre.y
+            
+            let label = UILabel()
+            label.text = "\(index + 1)"
+            
+            label.font = UIFont(name: "Arial", size: 20)
+            label.textColor = UIColor.black
+            label.sizeToFit()
+            
+            label.center = CGPoint(x: x, y: y)
+            
+            sipleclock2.addSubview(label)
+            angle += step
+            
+        }
+       
+            
+        }
+        
+    
 }
 

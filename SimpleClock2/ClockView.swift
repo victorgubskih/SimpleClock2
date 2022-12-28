49af4a10af0c07f2ae986217e4652033d027bab9
victorgubskih <112268920+victorgubskih@users.noreply.github.com>
2022-12-28 14:40:36 +0200

    create method hands

--- SimpleClock2/ClockView.swift
+++ SimpleClock2/ClockView.swift
@@ -8,15 +8,18 @@
 import UIKit
 
 class ClockView: UIView {
+    weak var hourArrow: UIView!
+    weak var minuteArrow: UIView!
+    weak var secondArrow: UIView!
 
     override var intrinsicContentSize: CGSize {
        return CGSize(width: 270, height: 270)
     }
+   
     func createOblectAraundCircle() {
-         
-         let centre = CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
-         let radius: CGFloat = 100
-         let count = 12
+        let centre = CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
+        let radius: CGFloat = 100
+        let count = 12
          
          var angle = CGFloat(2 * Double.pi - Double.pi / 3)
          let step = CGFloat( 2 * Double.pi) / CGFloat(count)
@@ -40,5 +43,37 @@ class ClockView: UIView {
              
          }
      
+    }
+    func createArrrowsAndClock() {
+        let hourArrow = UIView()
+        hourArrow.frame.size = CGSize(width: 16, height: 70)
+        hourArrow.layer.borderWidth = 1
+        hourArrow.layer.borderColor = UIColor.black.cgColor
+        hourArrow.layer.cornerRadius = 10
+        hourArrow.backgroundColor = UIColor.black
+        hourArrow.center = CGPoint(x: self.frame.width/2, y: self.frame.height/2 - hourArrow.frame.height / 2)
+        self.addSubview(hourArrow)
+        self.hourArrow = hourArrow
+       
+        let minuteArrow = UIView()
+        minuteArrow.frame.size = CGSize(width: 8, height: 90)
+        minuteArrow.layer.borderWidth = 1
+        minuteArrow.layer.borderColor = UIColor.green.cgColor
+        minuteArrow.layer.cornerRadius = 10
+        minuteArrow.backgroundColor = UIColor.green
+        minuteArrow.center = CGPoint(x: self.frame.width/2, y: self.frame.height/2 - minuteArrow.frame.height / 2)
+        self.addSubview(minuteArrow)
+        self.minuteArrow = minuteArrow
+        
+        let secondArrow = UIView()
+        secondArrow.frame.size = CGSize(width: 4, height: 95)
+        secondArrow.layer.borderWidth = 1
+        secondArrow.layer.borderColor = UIColor.red.cgColor
+        secondArrow.layer.cornerRadius = 10
+        secondArrow.backgroundColor = UIColor.red
+        secondArrow.center = CGPoint(x: self.frame.width/2, y: self.frame.height/2 - secondArrow.frame.height / 2)
+        self.addSubview(secondArrow)
+        self.secondArrow = secondArrow
+        
     }
 }
--- SimpleClock2/ViewController.swift
+++ SimpleClock2/ViewController.swift
@@ -8,34 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     var simpleClock: ClockView!
-    
-    var centreCirckle: UIView!
-   
-    
-    weak var hourClock: UIView!
-    
-    weak var minuteHandClock: UIView!
-    
-   
-    weak var secondHandClock: UIView!
-    
-   
-    
     var timer = Timer()
-    
-    
-    
-    
     var second = 60
     var minute = 60
     var hour = 12
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         simpleClock = ClockView()
         simpleClock.frame.size = CGSize(width: 270, height: 270)
         simpleClock.layer.cornerRadius = simpleClock.frame.height / 2
@@ -45,37 +26,11 @@ class ViewController: UIViewController {
         simpleClock.backgroundColor = UIColor.lightGray
         view.addSubview(simpleClock)
         
-        let hourClock = UIView()
-        hourClock.frame.size = CGSize(width: 16, height: 70)
-        hourClock.layer.borderWidth = 1
-        hourClock.layer.borderColor = UIColor.black.cgColor
-        hourClock.layer.cornerRadius = 10
-        hourClock.backgroundColor = UIColor.black
-        hourClock.center = CGPoint(x: simpleClock.frame.width/2, y: simpleClock.frame.height/2 - hourClock.frame.height / 2)
-        simpleClock.addSubview(hourClock)
-        self.hourClock = hourClock
-       
-        let minuteHandClock = UIView()
-        minuteHandClock.frame.size = CGSize(width: 8, height: 90)
-        minuteHandClock.layer.borderWidth = 1
-        minuteHandClock.layer.borderColor = UIColor.green.cgColor
-        minuteHandClock.layer.cornerRadius = 10
-        minuteHandClock.backgroundColor = UIColor.green
-        minuteHandClock.center = CGPoint(x: simpleClock.frame.width/2, y: simpleClock.frame.height/2 - minuteHandClock.frame.height / 2)
-        simpleClock.addSubview(minuteHandClock)
-        self.minuteHandClock = minuteHandClock
+        simpleClock.createOblectAraundCircle()
         
-        let secondHandClock = UIView()
-        secondHandClock.frame.size = CGSize(width: 4, height: 95)
-        secondHandClock.layer.borderWidth = 1
-        secondHandClock.layer.borderColor = UIColor.red.cgColor
-        secondHandClock.layer.cornerRadius = 10
-        secondHandClock.backgroundColor = UIColor.red
-        secondHandClock.center = CGPoint(x: simpleClock.frame.width/2, y: simpleClock.bounds.height/2 - secondHandClock.frame.height / 2)
-        simpleClock.addSubview(secondHandClock)
-        self.secondHandClock = secondHandClock
+        simpleClock.createArrrowsAndClock()
         
-        let centreCircle = ClockView()
+        let centreCircle = UIView()
         centreCircle.frame.size = CGSize(width: 20, height: 20)
         centreCircle.layer.cornerRadius = centreCircle.frame.height / 2
         centreCircle.center = view.center
@@ -84,62 +39,15 @@ class ViewController: UIViewController {
         centreCircle.backgroundColor = UIColor.orange
         view.addSubview(centreCircle)
        
-       
-       
+        movementHandClock(count: 12, index: 0, handView: simpleClock.hourArrow)
+        movementHandClock(count: 60, index: 0, handView: simpleClock.minuteArrow)
+        movementHandClock(count: 60, index: 0, handView: simpleClock.secondArrow)
         
-        
-        
-        movementHandClock(count: 12, index: 0, handView: hourClock)
-        movementHandClock(count: 60, index: 0, handView: minuteHandClock)
-        movementHandClock(count: 60, index: 0, handView: secondHandClock)
-        
-        
-       
-        
-        //timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
-        
-        
-        
-        simpleClock.createOblectAraundCircle()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
     }
 
-    func createOblectAraundCircle() {
-        
-        let centre = CGPoint(x: simpleClock.bounds.width / 2, y: simpleClock.bounds.height / 2)
-        let radius: CGFloat = 100
-        let count = 12
-        
-        var angle = CGFloat(2 * Double.pi - Double.pi / 3)
-        let step = CGFloat( 2 * Double.pi) / CGFloat(count)
-        
-        for index in 0..<count {
-            
-            let x = cos(angle) * radius + centre.x
-            let y = sin(angle) * radius + centre.y
-            
-            let label = UILabel()
-            label.text = "\(index + 1)"
-            
-            label.font = UIFont(name: "Arial", size: 20)
-            label.textColor = UIColor.black
-            label.sizeToFit()
-            
-            label.center = CGPoint(x: x, y: y)
-            
-            simpleClock.addSubview(label)
-            angle += step
-            
-        }
-    
-    
-   
-    
-            
-    }
-    
     func movementHandClock(count: Int, index: Int, handView: UIView) {
         let step = CGFloat(2 * Double.pi / CGFloat(count))
-        
         let x = handView.bounds.width / 4
         let y = handView.bounds.height / 2
         var transform = CGAffineTransform(translationX: x, y: y)
@@ -148,28 +56,29 @@ class ViewController: UIViewController {
         handView.transform = transform
         
     }
+    
     @objc func dataPickerAction(sender: UIDatePicker) {
         let date = sender.date
         let components = Calendar.current.dateComponents([.hour, .minute, .second], from: date)
         let hour = components.hour ?? 0
         let minute = components.minute ?? 0
         let second = components.second ?? 0
-        movementHandClock(count: 12, index: hour, handView: hourClock)
-        movementHandClock(count: 60, index: minute, handView: minuteHandClock)
-        movementHandClock(count: 60, index: second, handView: secondHandClock)
-            
+        movementHandClock(count: 12, index: hour, handView: simpleClock.hourArrow)
+        movementHandClock(count: 60, index: minute, handView: simpleClock.minuteArrow)
+        movementHandClock(count: 60, index: second, handView: simpleClock.secondArrow)
     }
+    
     @objc func timerAction() {
         second += 1
-        movementHandClock(count: 60, index: second, handView: secondHandClock)
+        movementHandClock(count: 60, index: second, handView: simpleClock.secondArrow)
         if second == 60  {
             second = 0
             minute += 1
-            movementHandClock(count: 60, index: minute, handView: minuteHandClock)
+            movementHandClock(count: 60, index: minute, handView: simpleClock.minuteArrow)
             if minute == 60 {
                 minute = 0
                 hour += 1
-                movementHandClock(count: 12, index: hour, handView: hourClock)
+                movementHandClock(count: 12, index: hour, handView: simpleClock.hourArrow)
             }
             
         }

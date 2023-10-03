984505b881d0e80fa6167a1ceaee86d9987b68cd
виктор <victor.gubskih@gmail.com>
2023-10-03 18:57:46 +0300

    create project

---
+++ SimpleClock2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleClock2
+//
+//  Created by Victor on 03.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

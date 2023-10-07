96c29aa271eb4708e4340913975c96fb932ce44a
виктор <victor.gubskih@gmail.com>
2023-10-07 19:38:00 +0300

    add button start/stop

--- SimpleClock2/ViewController.swift
+++ SimpleClock2/ViewController.swift
@@ -9,13 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet private(set) var timeLabel: UILabel!
+    var button = UIButton()
+    var timer = Timer()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
        
+      
         updateTimeLabel()
+        
+        view.addSubview(button)
+        button.frame = CGRect(x: 0, y: view.frame.height - 300, width: 110, height: 48)
+        button.setTitle("Stop", for: .normal)
+        button.center.x = view.center.x
+        button.setTitleColor(.red, for: .normal)
+        button.addTarget(self, action: #selector(buttonAction(sender:)), for: .touchUpInside)
+        
     }
     
     @objc func fireTimer() {
@@ -26,9 +38,20 @@ class ViewController: UIViewController {
         let currentDate = Date()
         let formatter = DateFormatter()
         formatter.dateFormat = "HH:mm:ss"
-        let timeStr = formatter.string(from: currentDate)
-        timeLabel.text = timeStr
+        let timeString = formatter.string(from: currentDate)
+        timeLabel.text = timeString
     }
+    @objc func buttonAction(sender: UIButton) {
+        if sender.title(for: .normal) == "Stop"  {
+            sender.setTitle("Start", for: .normal)
+            timer.invalidate()
+        } else {
+            sender.setTitle("Stop", for: .normal)
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
+        }
+    }
+    
+   
     
 }
 

53f3cf55592e78a34a496586700233f0f156abff
виктор <victor.gubskih@gmail.com>
2023-10-13 17:58:29 +0300

    create table view

--- SimpleClock2/TimeZoneController.swift
+++ SimpleClock2/TimeZoneController.swift
@@ -13,54 +13,45 @@ protocol UpdateTimeZoneDelegateProtocol  {
     func upgateTimeViewControler(timeZone: TimeZone)
 }
 
-class TimeZoneController: UIViewController {
+class TimeZoneController: UIViewController  {
     
     var delegate: UpdateTimeZoneDelegateProtocol? = nil
-    
-    var localButton = UIButton(type: .roundedRect)
-    var losAgelesButton = UIButton(type: .roundedRect)
+    let tableView = UITableView()
+    var safeArea: UILayoutGuide!
+    var characters = ["Los Angeles", "Local"]
+   
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(localButton)
-        localButton.setTitle("Local", for: .normal)
-        localButton.setTitleColor(.red, for: .normal)
-        localButton.addTarget(self, action: #selector(localAction(sender:)), for: .touchUpInside)
-        localButton.backgroundColor = .gray
-        localButton.layer.cornerRadius = 10
-        localButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            localButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            localButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -93),
-            localButton.widthAnchor.constraint(equalToConstant: 110),
-            localButton.heightAnchor.constraint(equalToConstant: 48)
-        ])
-        
-        view.addSubview(losAgelesButton)
-        losAgelesButton.setTitle("Los Angeles", for: .normal)
-        losAgelesButton.setTitleColor(.green, for: .normal)
-        losAgelesButton.addTarget(self, action: #selector(losAngelesAction(sender:)), for: .touchUpInside)
-        losAgelesButton.backgroundColor = .gray
-        losAgelesButton.layer.cornerRadius = 10
-        losAgelesButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            losAgelesButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            losAgelesButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -93),
-            losAgelesButton.widthAnchor.constraint(equalToConstant: 110),
-            losAgelesButton.heightAnchor.constraint(equalToConstant: 48)
-        ])
-        
-    }
-
-    @objc func localAction(sender: UIButton) {
-        dismiss(animated: true, completion: nil)
-        self.delegate?.upgateTimeViewControler(timeZone: TimeZone.current)
+        view.backgroundColor = .white
+        safeArea = view.layoutMarginsGuide
+        setupTableView()
     }
     
-    @objc func losAngelesAction(sender: UIButton) {
-        dismiss(animated: true, completion: nil)
-        self.delegate?.upgateTimeViewControler(timeZone: TimeZone(identifier: "UTC-7")!)
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableView.dataSource = self
     }
-
+        
+    
+}
+extension TimeZoneController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = characters[indexPath.row]
+        return cell
+    }
 }

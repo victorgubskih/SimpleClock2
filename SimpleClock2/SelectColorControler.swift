4bf91ca33ae39d3b3e13101b7399cc73664b0420
виктор <victor.gubskih@gmail.com>
2023-10-24 16:49:54 +0300

    add tableView inside SelectColorControler

--- SimpleClock2/SelectColorControler.swift
+++ SimpleClock2/SelectColorControler.swift
@@ -12,69 +12,45 @@ protocol UpdateColorDelegateProtocol: AnyObject {
 }
 
 class SelectColorControler: UIViewController {
-    
+    private let tableView = UITableView()
     var delegate: UpdateColorDelegateProtocol? = nil
-    var buttonColorRed = UIButton(type: .roundedRect)
-    var buttonColorBlue = UIButton(type: .roundedRect)
-    var buttonColorGreen = UIButton(type: .roundedRect)
+    private var colors: [String] = ["Red", "Green", "Blue"]
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.addSubview(buttonColorRed)
-        buttonColorRed.setTitle("Red", for: .normal)
-        buttonColorRed.setTitleColor(.red, for: .normal)
-        buttonColorRed.addTarget(self, action: #selector(redButtonAction(sender:)), for: .touchUpInside)
-        buttonColorRed.backgroundColor = .gray
-        buttonColorRed.layer.cornerRadius = 10
-        buttonColorRed.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            buttonColorRed.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
-            buttonColorRed.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
-            buttonColorRed.widthAnchor.constraint(equalToConstant: 100),
-            buttonColorRed.heightAnchor.constraint(equalToConstant: 50)
-        ])
         
-        view.addSubview(buttonColorBlue)
-        buttonColorBlue.setTitle("Blue", for: .normal)
-        buttonColorBlue.setTitleColor(.blue, for: .normal)
-        buttonColorBlue.addTarget(self, action: #selector(blueButtonAction(sender:)), for: .touchUpInside)
-        buttonColorBlue.backgroundColor = .gray
-        buttonColorBlue.layer.cornerRadius = 10
-        buttonColorBlue.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            buttonColorBlue.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
-            buttonColorBlue.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
-            buttonColorBlue.widthAnchor.constraint(equalToConstant: 100),
-            buttonColorBlue.heightAnchor.constraint(equalToConstant: 50)
-        ])
+        setupTableView()
+        tableView.reloadData()
+    }
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         
-        view.addSubview(buttonColorGreen)
-        buttonColorGreen.setTitle("Green", for: .normal)
-        buttonColorGreen.setTitleColor(.green, for: .normal)
-        buttonColorGreen.addTarget(self, action: #selector(greenButtonAction(sender:)), for: .touchUpInside)
-        buttonColorGreen.backgroundColor = .gray
-        buttonColorGreen.layer.cornerRadius = 10
-        buttonColorGreen.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            buttonColorGreen.trailingAnchor.constraint(equalTo: buttonColorRed.leadingAnchor, constant: -40),
-            buttonColorGreen.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
-            buttonColorGreen.widthAnchor.constraint(equalToConstant: 100),
-            buttonColorGreen.heightAnchor.constraint(equalToConstant: 50)
-        ])
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableView.dataSource = self
+       // tableView.delegate = self
     }
 
 
-    @objc func redButtonAction(sender: UIButton) {
-        self.delegate?.buttonTapedAt(color: .red)
-        dismiss(animated: true, completion: nil)
+   
+}
+
+extension SelectColorControler: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
     }
-    @objc func blueButtonAction(sender: UIButton) {
-        self.delegate?.buttonTapedAt(color: .blue)
-        dismiss(animated: true, completion: nil)
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = colors[indexPath.row]
+        return cell
     }
-    @objc func greenButtonAction(sender: UIButton) {
-        self.delegate?.buttonTapedAt(color: .green)
-        dismiss(animated: true, completion: nil)
-    }
+    
+ 
 }
--- SimpleClock2/TimeZoneController.swift
+++ SimpleClock2/TimeZoneController.swift
@@ -26,6 +26,7 @@ class TimeZoneController: UIViewController  {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         view.backgroundColor = .white
         safeArea = view.layoutMarginsGuide
         setupTableView()

858f7672ac8d51d3c04a62c8e67c910d86053b12
виктор <victor.gubskih@gmail.com>
2023-10-30 15:10:57 +0200

    adding new color

--- SimpleClock2/SelectColorControler.swift
+++ SimpleClock2/SelectColorControler.swift
@@ -11,10 +11,20 @@ protocol UpdateColorDelegateProtocol: AnyObject {
     func buttonTapedAt(color: UIColor)
 }
 
+enum SelectColorItem {
+    case color(UIColor)
+    case addNew(String)
+}
+
 class SelectColorControler: UIViewController {
     private let tableView = UITableView()
     var delegate: UpdateColorDelegateProtocol? = nil
-    private var colors: [(String, UIColor)] = [("Red", UIColor.red), ("Green", UIColor.green), ("Blue", UIColor.blue)]
+    private var items: [SelectColorItem] = [
+        .color(UIColor.red),
+        .color(UIColor.green),
+        .color(UIColor.blue),
+        .addNew("Add new color")
+    ]
     
 
     override func viewDidLoad() {
@@ -43,23 +53,56 @@ class SelectColorControler: UIViewController {
 
 extension SelectColorControler: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return colors.count
+        return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = (colors[indexPath.row]).0
+        /*if indexPath.row < colors.count {
+            cell.backgroundColor = (colors[indexPath.row])
+        } else {
+            cell.textLabel?.text = "add new color"
+        }*/
+        switch items[indexPath.row] {
+        case .color(let c):
+            cell.backgroundColor = c
+            cell.textLabel?.text = nil
+        case .addNew(let t):
+            cell.textLabel?.text = t
+            cell.backgroundColor = .white
+        }
         return cell
     }
     
- 
+   
 }
 
 extension SelectColorControler: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        dismiss(animated: true, completion: nil)
-        let color = colors[indexPath.row].1
-        delegate?.buttonTapedAt(color: color)
-        
+        /*if indexPath.row < colors.count {
+            dismiss(animated: true, completion: nil)
+            let color = colors[indexPath.row]
+            delegate?.buttonTapedAt(color: color)
+        }*/
+        switch items[indexPath.row] {
+        case .color(let c):
+            dismiss(animated: true, completion: nil)
+            delegate?.buttonTapedAt(color: c)
+        case .addNew(let t):
+            let colorPickerVC = UIColorPickerViewController()
+            colorPickerVC.delegate = self
+            present(colorPickerVC, animated: true)
+        }
     }
+}
+extension SelectColorControler : UIColorPickerViewControllerDelegate {
+
+    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
+        let selectedColor = viewController.selectedColor
+        items.append(SelectColorItem.color(selectedColor))
+        tableView.reloadData()
+    }
+    
+    
+
 }

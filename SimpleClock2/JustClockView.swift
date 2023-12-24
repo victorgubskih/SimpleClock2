1b320650291372e316b8f76080b80a378fe2fd64
виктор <victor.gubskih@gmail.com>
2023-12-24 23:10:00 +0200

    create ImageView minute, hour hands

--- SimpleClock2/JustClockView.swift
+++ SimpleClock2/JustClockView.swift
@@ -11,15 +11,12 @@ import UIKit
 class JustClockView: UIView {
     
     @IBOutlet private(set) var circlelView1: UIView!
-
-    @IBOutlet private(set) var secondHandView: UIView!
+    
     @IBOutlet private(set) var secondsHandsContainer: UIView!
     
-    @IBOutlet private(set) var minuteHandsContainer: UIView!
-    @IBOutlet private(set) var minuteHandView: UIView!
+    @IBOutlet private(set) var minuteHandsContainer2: UIView!
     
-    @IBOutlet private(set) var hourHandsContainer: UIView!
-    @IBOutlet private(set) var hourHandView: UIView!
+    @IBOutlet private(set) var hourHandsContainer2: UIView!
     var calendar = Calendar.current
     
     required init?(coder: NSCoder) {
@@ -48,23 +45,13 @@ class JustClockView: UIView {
         view.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor).isActive = true
         view.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor).isActive = true
         
-        hourHandView.layer.cornerRadius = 10
-        hourHandView.layer.borderWidth = 1.0
-        hourHandView.clipsToBounds = true
-        
-        minuteHandView.layer.cornerRadius = 10
-        minuteHandView.layer.borderWidth = 1.0
-        minuteHandView.clipsToBounds = true
-        
-        secondHandView.layer.cornerRadius = 10
-        secondHandView.layer.borderWidth = 1.0
-        secondHandView.clipsToBounds = true
+       
     }
 }
 
 extension JustClockView: ClockViewProtocol {
     func currentColor() -> UIColor {
-        return hourHandView.backgroundColor ?? .clear
+        return self.backgroundColor ?? .clear
     }
     
     func updateTime() {
@@ -73,12 +60,16 @@ extension JustClockView: ClockViewProtocol {
         let components = calendar.dateComponents([.second, .minute, .hour], from: currentDate)
         
         let hours = CGFloat(components.hour ?? 0)
-        hourHandsContainer.transform = CGAffineTransform(rotationAngle: (2 * CGFloat.pi * hours / 12.0))
+        
+        hourHandsContainer2.transform = CGAffineTransform(rotationAngle: (2 * CGFloat.pi * hours / 12.0))
        
         let minutes = CGFloat(components.minute ?? 0)
-        minuteHandsContainer.transform = CGAffineTransform(rotationAngle: (2 * CGFloat.pi * minutes / 60.0))
+        
+        minuteHandsContainer2.transform = CGAffineTransform(rotationAngle: (2 * CGFloat.pi * minutes / 60.0))
         
         let seconds = CGFloat(components.second ?? 0)
+       
+        
         secondsHandsContainer.transform = CGAffineTransform(rotationAngle: (2 * CGFloat.pi * seconds / 60.0))
        
     }
@@ -89,9 +80,7 @@ extension JustClockView: ClockViewProtocol {
     }
     
     func update(color: UIColor) {
-        hourHandView.backgroundColor = color
-        minuteHandView.backgroundColor = color
-        secondHandView.backgroundColor = color
+        self.backgroundColor = color
         
     }
     
